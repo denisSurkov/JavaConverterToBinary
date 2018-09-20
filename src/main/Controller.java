0be970ac971bb727e1d117d44f9de23a5445612c
denissurkov <surkov0denis@yandex.ru>
2018-09-20 17:08:44 +0500

    add type support

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -33,27 +34,79 @@ public class Controller {
     private Label errorLabel;
 
     @FXML
+    private Label labelCanBeByte;
+
+    @FXML
+    private Label labelCanBeShort;
+
+    @FXML
+    private Label labelCanBeInt;
+
+    @FXML
     void initialize() {
-        Converter converter = new Converter();
 
         sumbitToConvert.setOnAction(event -> {
-            outputConvertedNum.clear();
-
-            String inputData = inputNumToConvert.getText();
-            System.out.println(inputData);
-
-            if (inputData.matches("-?[0-9]+")) {
-                long intFromUser = Long.parseLong(inputData);
-
-                String result = converter.convertNumberToBin(intFromUser);
-                outputConvertedNum.setText(result);
+            if (checkCanConvert()){
+                setResult();
+                setCheckedTypes();
             } else {
-
-                inputNumToConvert.clear();
-                errorLabel.setVisible(true);
+                setError();
             }
-
         });
+    }
+
+    private void setError(){
+        inputNumToConvert.clear();
+        errorLabel.setVisible(true);
+    }
+
+    private boolean checkCanConvert(){
+        String inputData = inputNumToConvert.getText();
+        System.out.println(inputData);
+
+        return inputData.matches("-?[0-9]+");
+    }
+
+    private void setResult(){
+        outputConvertedNum.clear();
+
+        Converter converter = new Converter();
+        String inputData = inputNumToConvert.getText();
+        long intFromUser = Long.parseLong(inputData);
+
+        String result = converter.convertNumberToBin(intFromUser);
+        outputConvertedNum.setText(result);
+    }
+
+    private void setCheckedTypes(){
+        String styleForUnsucess = getStyleForLabels(false),
+                styleForSucess = getStyleForLabels(true);
+
+
+        labelCanBeByte.setStyle(styleForUnsucess);
+        labelCanBeShort.setStyle(styleForUnsucess);
+        labelCanBeInt.setStyle(styleForUnsucess);
+
+        String inputData = inputNumToConvert.getText();
+        long intFromUser = Long.parseLong(inputData);
+
+        if (Converter.checkCanBeByte(intFromUser)){
+            labelCanBeByte.setStyle(styleForSucess);
+        }
+
+        if (Converter.checkCanBeShort(intFromUser)){
+            labelCanBeShort.setStyle(styleForSucess);
+        }
+
+        if (Converter.checkCanBeInt(intFromUser)){
+            labelCanBeInt.setStyle(styleForSucess);
+        }
+
+    }
+
+    private String getStyleForLabels(boolean success){
+        return success ? "-fx-background-color:  #42f448; -fx-background-radius: 45;" :
+                "-fx-background-color:   #f45042; -fx-background-radius: 45;";
     }
 
 }
--- src/main/Converter.java
+++ src/main/Converter.java
@@ -20,42 +20,37 @@ public class Converter {
         StringBuilder answer = new StringBuilder();
 
         // TODO: Сделать поддержку отрицательных чисел.
-        long x = Math.abs(numToConvert);
+        long x = Math.abs(numToConvert), res = x;
 
-        while (x >= 1) {
-            long res = x >> BIN_STEP; // Деление на 2 (побитовый сдвиг вправа)
+        while (x > 0) {
 
             if (res % 2 == 0) {
-                answer.append(0);
+                answer.insert(0, 0);
             } else {
-                answer.append(1);
+                answer.insert(0, 1);
             }
 
+            res = x >> BIN_STEP; // Деление на 2 (побитовый сдвиг вправа)
             x = res;
+        }
+
+        if (numToConvert % 2 == 1){
+            answer.replace(answer.length() - 1, answer.length(), "1");
         }
 
         return answer.toString();
     }
 
     public static boolean checkCanBeByte(long x){
-        if (x <= 127 && x >= -128){
-            return true;
-        }
-        return false;
+        return (x <= 127) && (x >= -128);
     }
 
     public static boolean checkCanBeShort(long x){
-        if (x >= -32768 && x <= 32767){
-            return true;
-        }
-        return false;
+        return x >= -32768 && x <= 32767;
     }
 
     public static boolean checkCanBeInt(long x){
-        if (x >= -2147483648 && x <= 2147483647){
-            return true;
-        }
-        return false;
+        return x >= -2147483648 && x <= 2147483647;
     }
 
 
